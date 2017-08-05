b91b84a2937c058a4ad1625201fd03f2fc9a9eff
Kevin Jayne <Kevin.Jayne@ADP.com>
2017-08-05 01:12:13 -0400

    update setfrom

--- main.go
+++ main.go
@@ -1,13 +1,40 @@
 package gtills
 
 import (
-  "bytes"
+	"bufio"
+	"fmt"
+	"os"
+	"os/user"
+	"strings"
 )
 
-func Concat(s1, s2 string) string{
-  var buffer bytes.Buffer
-  buffer.WriteString(s1)
-  buffer.WriteString(" ")
-  buffer.WriteString(s2)
-  return buffer.String()
+// E : exit with error code 1 and print if err is notnull
+func E(err error, msg string) {
+	if err != nil {
+		fmt.Println("❌  "+msg, err)
+		os.Exit(1)
+	}
+}
+
+func GetHomeDir() string {
+	usr, err := user.Current()
+	if err == nil {
+		return usr.HomeDir
+	} else {
+		// Maybe it's cross compilation without cgo support. (darwin, unix)
+		return os.Getenv("HOME")
+	}
+}
+
+func GetInput(q string) string {
+	if q != "" {
+		fmt.Print(q)
+	}
+	reader := bufio.NewReader(os.Stdin)
+	ans, _ := reader.ReadString('\n')
+	return strings.TrimRight(ans, "\n")
+}
+
+func SetFromInput(a *string, q string) {
+	*a = strings.TrimRight(GetInput(q), "\n")
 }
