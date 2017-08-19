6a5f1ef76909acc75b17a2e3f50c2ae71e70d2e9
Kevin Jayne <Kevin.Jayne@ADP.com>
2017-08-18 22:13:07 -0400

    revert

--- gtils.go
+++ gtils.go
@@ -10,9 +10,44 @@ import (
 	"net/http"
 	"os"
 	"os/user"
-	"strconv"
 	"strings"
 )
+
+// Loop2D : loop through 2 dimentional slice with 2 nested for loops uwing provided width and height
+func Loop2D(height, width int, logic func(row, col int)) {
+	for row := 0; row < height; row++ {
+		for col := 0; col < width; col++ {
+			logic(row, col)
+		}
+	}
+}
+
+// Mkdir : make a directory if it does not exist
+func Mkdir(filePath string) {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		os.Mkdir(filePath, os.ModePerm)
+	}
+}
+
+// DownloadImage : download image from provided url and save to provided filelocation
+func DownloadImage(url, fileName string) {
+	response, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	LoE("Error Getting Image "+url, err)
+
+	defer response.Body.Close()
+
+	//open a file for writing
+	file, err := os.Create(fileName)
+	LoE("Error Creating File", err)
+
+	// Use io.Copy to just dump the response body to the file. This supports huge files
+	_, err = io.Copy(file, response.Body)
+	LoE("Error Saving Image File", err)
+	file.Close()
+}
 
 // SendRequest : send http request to provided url
 func SendRequest(req *http.Request) []byte {
@@ -21,7 +56,7 @@ func SendRequest(req *http.Request) []byte {
 	EoE("Error Getting HTTP Response", err)
 
 	resData, err := ioutil.ReadAll(res.Body)
-	EoE("Error Parsing HTTP Response", err)
+	EoE("Error Parsing Response", err)
 	return resData
 }
 
@@ -30,6 +65,14 @@ func EoE(msg string, err error) {
 	if err != nil {
 		fmt.Printf("\n❌  %s\n   %v\n", msg, err)
 		os.Exit(1)
+	}
+}
+
+// LoE : exit with error code 1 and print if err is notnull
+func LoE(msg string, err error) {
+	if err != nil {
+		fmt.Printf("\n❌  %s\n   %v\n", msg, err)
+		// log.Fatal(err)
 	}
 }
 
@@ -65,24 +108,12 @@ func Confirm(q string) bool {
 
 // GetInput : return string of user input
 func GetInput(q string) string {
-	print(q)
+	if q != "" {
+		print(q)
+	}
 	reader := bufio.NewReader(os.Stdin)
 	ans, _ := reader.ReadString('\n')
 	return strings.TrimRight(ans, "\n")
-}
-
-// SelectFromArray : select an element in the provided array
-func SelectFromArray(a []string) string {
-	fmt.Println("Choices:")
-	for i := range a {
-		fmt.Println("[", i, "]: "+a[i])
-	}
-	sel, err := strconv.Atoi(GetInput("Enter Number of Selection: "))
-	EoE("Error Getting Integer Input from User", err)
-	if sel <= len(a)-1 {
-		return a[sel]
-	}
-	return SelectFromArray(a)
 }
 
 // SetFromInput : set value of provided var to the value of user input
