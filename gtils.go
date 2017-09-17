411a4eaeba224f6db94948cf64efdb2846ef7aaf
Kevin Jayne <Kevin.Jayne@ADP.com>
2017-09-17 12:07:32 -0400

    add ImportPathToDir

--- gtils.go
+++ gtils.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
+	"go/build"
 	"io"
 	"io/ioutil"
 	"log"
@@ -16,6 +17,17 @@ import (
 	"strings"
 	"time"
 )
+
+// ImportPathToDir : resolves the absolute path from importPath.
+// There doesn't need to be a valid Go package inside that import path,
+// but the directory must exist.
+func ImportPathToDir(importPath string) (string, error) {
+	p, err := build.Import(importPath, "", build.FindOnly)
+	if err != nil {
+		return "", err
+	}
+	return p.Dir, nil
+}
 
 // readShaderFile : read the shader file and return null terminated string
 func readFile(filePath string) string {
