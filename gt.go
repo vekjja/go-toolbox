d1f8855edbc1ea16e44a0d5e5327ce79043d9ec4
Kevin Jayne <kevin.jayne@wework.com>
2018-01-17 22:32:36 -0500

    cleanup

--- gt.go
+++ gt.go
@@ -13,6 +13,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"os/exec"
 	"os/user"
 	"path/filepath"
 	"regexp"
@@ -219,4 +220,24 @@ func ExecPath() string {
 	}
 	exPath := filepath.Dir(ex)
 	return exPath
+}
+
+// OpenBrowser : open platform specific browser
+func OpenBrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
