160baf52618c316da0ace1ba7eaf62f415ad33bb
seemywingz <kevin.jayne@icloud.com>
2020-01-15 00:48:48 -0500

    add some helper functions

--- gt.go
+++ gt.go
@@ -270,3 +270,46 @@ func ReadVar(file string, object interface{}) error {
 	gobFile.Close()
 	return nil
 }
+
+// GetIP : get local ip address
+func getPubIP() string {
+	// we are using a pulib IP API, we're using ipify here, below are some others
+	// https://www.ipify.org
+	// http://myexternalip.com
+	// http://api.ident.me
+	// http://whatismyipaddress.com/api
+	// https://ifconfig.co
+	// https://ifconfig.me
+	url := "https://api.ipify.org?format=text"
+	resp, err := http.Get(url)
+	EoE("Error Getting IP Address", err)
+	defer resp.Body.Close()
+	ip, err := ioutil.ReadAll(resp.Body)
+	EoE("Error Reading IP Address", err)
+	return string(ip)
+}
+
+func epochFormat(seconds int64) string {
+	epochTime := time.Unix(0, seconds*int64(time.Second))
+	return epochTime.Format("January 2, 3:04pm MST")
+}
+
+func epochFormatDate(seconds int64) string {
+	epochTime := time.Unix(0, seconds*int64(time.Second))
+	return epochTime.Format("January 2 (Monday)")
+}
+
+func epochFormatTime(seconds int64) string {
+	epochTime := time.Unix(0, seconds*int64(time.Second))
+	return epochTime.Format("3:04pm MST")
+}
+
+func epochFormatHour(seconds int64) string {
+	epochTime := time.Unix(0, seconds*int64(time.Second))
+	s := epochTime.Format("3pm")
+	s = s[:len(s)-1]
+	if len(s) == 2 {
+		s += " "
+	}
+	return s
+}
