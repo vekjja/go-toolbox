c70a49d36fef65ca1aba2e9075d949ac331d6690
Kevin Jayne <kevin.jayne@icloud.com>
2020-01-24 21:41:14 -0500

    epoch

--- epoch/epoch.go
+++ epoch/epoch.go
@@ -1,23 +1,27 @@
-package time
+package epoch
 
 import "time"
 
-func epochFormat(seconds int64) string {
+// Format : format epoch -- "January 2, 3:04pm MST"
+func Format(seconds int64) string {
 	epochTime := time.Unix(0, seconds*int64(time.Second))
 	return epochTime.Format("January 2, 3:04pm MST")
 }
 
-func epochFormatDate(seconds int64) string {
+// FormatDate : format epoch -- "January 2 (Monday)"
+func FormatDate(seconds int64) string {
 	epochTime := time.Unix(0, seconds*int64(time.Second))
 	return epochTime.Format("January 2 (Monday)")
 }
 
-func epochFormatTime(seconds int64) string {
+// FormatTime : format epoch -- "3:04pm MST"
+func FormatTime(seconds int64) string {
 	epochTime := time.Unix(0, seconds*int64(time.Second))
 	return epochTime.Format("3:04pm MST")
 }
 
-func epochFormatHour(seconds int64) string {
+// FormatHour : format epoch -- "3pm"
+func FormatHour(seconds int64) string {
 	epochTime := time.Unix(0, seconds*int64(time.Second))
 	s := epochTime.Format("3pm")
 	s = s[:len(s)-1]
