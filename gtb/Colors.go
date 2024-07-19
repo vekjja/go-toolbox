34dea06dcf0ce6cf6dd6ef11b90422d97e77ba8b
seemywings <kevin.jayne@icloud.com>
2024-07-19 11:27:59 -0400

    colors

---
+++ gtb/Colors.go
@@ -0,0 +1,27 @@
+package gtb
+
+import "github.com/fatih/color"
+
+var Red = color.New(color.FgRed).SprintFunc()
+var Green = color.New(color.FgGreen).SprintFunc()
+var Blue = color.New(color.FgBlue).SprintFunc()
+var Yellow = color.New(color.FgYellow).SprintFunc()
+var Magenta = color.New(color.FgMagenta).SprintFunc()
+var Cyan = color.New(color.FgCyan).SprintFunc()
+var White = color.New(color.FgWhite).SprintFunc()
+var Black = color.New(color.FgBlack).SprintFunc()
+
+var HiRed = color.New(color.FgHiRed).SprintFunc()
+var HiGreen = color.New(color.FgHiGreen).SprintFunc()
+var HiBlue = color.New(color.FgHiBlue).SprintFunc()
+var HiYellow = color.New(color.FgHiYellow).SprintFunc()
+var HiMagenta = color.New(color.FgHiMagenta).SprintFunc()
+var HiCyan = color.New(color.FgHiCyan).SprintFunc()
+var HiWhite = color.New(color.FgHiWhite).SprintFunc()
+var HiBlack = color.New(color.FgHiBlack).SprintFunc()
+
+var Bold = color.New(color.Bold).SprintFunc()
+var Faint = color.New(color.Faint).SprintFunc()
+var Italic = color.New(color.Italic).SprintFunc()
+var Underline = color.New(color.Underline).SprintFunc()
+var Blink = color.New(color.BlinkSlow).SprintFunc()
--- gtb/Errors.go
+++ gtb/Errors.go
@@ -1,6 +1,7 @@
 package gtb
 
 import (
+	"errors"
 	"fmt"
 	"os"
 )
@@ -10,8 +11,19 @@ func printError(err error) {
 }
 
 // EoE : if err is not nil print error and exit
-func EoE(err error) {
+func EoE(err error, msg ...string) {
 	if err != nil {
+		if len(msg) > 0 {
+			errMsg := ""
+			for i, m := range msg {
+				if i > 0 {
+					errMsg += "   " + m + "\n"
+				} else {
+					errMsg += Red(m) + "\n"
+				}
+			}
+			err = errors.New(errMsg)
+		}
 		printError(err)
 		os.Exit(1)
 		panic(err)
