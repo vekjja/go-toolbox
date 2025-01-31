7064199b0f89709259ac9ba4d7d61c3416c43a82
seemywings <kevin.jayne@icloud.com>
2025-01-31 12:24:20 -0500

    time

--- Epoch.go
+++ Epoch.go
@@ -2,31 +2,14 @@ package toolbox
 
 import "time"
 
-// Format : format epoch -- "January 2, 3:04pm MST"
-func Format(seconds int64) string {
-	epochTime := time.Unix(0, seconds*int64(time.Second))
-	return epochTime.Format("January 2, 3:04pm MST")
-}
-
-// FormatDate : format epoch -- "January 2 (Monday)"
-func FormatDate(seconds int64) string {
-	epochTime := time.Unix(0, seconds*int64(time.Second))
-	return epochTime.Format("January 2 (Monday)")
-}
-
-// FormatTime : format epoch -- "3:04pm MST"
-func FormatTime(seconds int64) string {
-	epochTime := time.Unix(0, seconds*int64(time.Second))
-	return epochTime.Format("3:04pm MST")
-}
-
-// FormatHour : format epoch -- "3pm"
-func FormatHour(seconds int64) string {
-	epochTime := time.Unix(0, seconds*int64(time.Second))
-	s := epochTime.Format("3pm")
-	s = s[:len(s)-1]
-	if len(s) == 2 {
-		s += " "
+func LocalTime(seconds int, offset int, timezone string, format string) string {
+	if format == "" {
+		format = "Mon, Jan, 2 - 03:04 PM"
 	}
-	return s
+	if timezone == "" {
+		timezone = "UTC"
+	}
+	loc := time.FixedZone(timezone, offset)
+	localTime := time.Unix(int64(seconds), 0).In(loc)
+	return localTime.Format(format)
 }
