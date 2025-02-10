9d5da7407e45aa1a409f07f858e8c275fe7cba65
seemywings <kevin.jayne@icloud.com>
2025-02-10 13:25:55 -0500

    dir

---
+++ Dir.go
@@ -0,0 +1,27 @@
+package toolbox
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"runtime"
+)
+
+// Mkdir : make a directory if it does not exist
+func Mkdir(filePath string) {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		os.Mkdir(filePath, os.ModePerm)
+	}
+}
+
+// SetRelPath : resolves the absolute path for provided relative path.
+func SetRelPath(relPath string) error {
+	if _, filename, _, ok := runtime.Caller(1); ok {
+		re := regexp.MustCompile("[a-zA-Z0-9-]*.go$")
+		path := filepath.Join(re.ReplaceAllString(filename, ""), relPath)
+		return os.Chdir(path)
+	} else {
+		return fmt.Errorf("Error Getting Caller Location: %s", filename)
+	}
+}
